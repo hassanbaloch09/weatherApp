41fe7184f19221ee9f9c26886dd0751fe198ecd9
hassanbaloch09 <hassan.balouch333@gmail.com>
2023-11-26 22:49:45 +0500

    weatherApp

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
+const APIKEY = "YourAPIKey";
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,7 +12,7 @@ app.get("/", function (req, res) {
 
 app.post("/", function (req, res) {
   const query = req.body.cityName;
-  const apiKey = "b289616c9a4e7a508bb998759b369e5c";
+  const apiKey = APIKEY;
   const unit = "metric";
   const url =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
